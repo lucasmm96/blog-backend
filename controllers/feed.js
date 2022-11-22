e22a815fda09e507fa66e5a758cf0c5f2826b33c
Lucas Martins Menezes <lucasma@br.ibm.com>
2022-11-22 16:53:13 -0300

    add creator and createdAt

--- controllers/feed.js
+++ controllers/feed.js
@@ -21,6 +21,6 @@ exports.createPost = (req, res, next) => {
   // Create post in db
   res.status(201).json({
     message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    post: { id: new Date().toISOString(), title: title, content: content, creator: { name: 'Lucas'}, createdAt: new Date() }
   });
 };
