4ffdce52f5408207a34eb8a454a16177c7dc4691
Lucas Martins Menezes <lucasma@br.ibm.com>
2022-12-01 09:49:46 -0300

    set graphql

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config();
 const multer = require('multer');
+const { graphqlHTTP } = require('express-graphql');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 
 const app = express();
 
@@ -35,6 +38,11 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('/graphql', graphqlHTTP({
+  schema: graphqlSchema,
+  rootValue: graphqlResolver,
+  graphiql: true
+}));
 
 app.use((error, req, res, next) => {
   console.log(error);
